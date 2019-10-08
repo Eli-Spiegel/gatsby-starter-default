a9459983685b1618771f2102658fb736399cf953
Eli Spiegel <eli@barn2door.com>
2019-10-08 15:50:24 -0700

    no message

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Eli's Gatsby`,
+    description: `Bare bones gatsby`,
     author: `@gatsbyjs`,
   },
   plugins: [
